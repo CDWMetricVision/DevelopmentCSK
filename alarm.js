716ddfbfbc1aca589973f8ba964c86de18038526
Hareesh-Raj <hareeshraja.ramanathan@cdw.com>
2025-01-28 16:23:15 +0530

    alarms-implemented alarms page and api handling

---
+++ alarm.js
@@ -0,0 +1,86 @@
+function getAccountsAlarmsAPI() {
+    const allAccountsAlarmsList = [
+        {
+            "MAS Sandbox Development": {
+                "cloudWatchAPI":"https://ps8du4fwof.execute-api.us-east-1.amazonaws.com/test/GetAlarmCloudwatch"
+            }
+        },
+        {
+            "MAS Sandbox Test1": {
+                "cloudWatchAPI":"https://ownc55gea9.execute-api.us-east-1.amazonaws.com/test/GetAlarmCloudwatch_test1"
+            }
+        },
+        {
+            "MAS Sandbox Test2": {
+                "cloudWatchAPI":"t16ya9cmcl.execute-api.us-east-1.amazonaws.com/test2/GetAlarmCloudwatch_test2"
+            }
+        }
+    ]
+    return allAccountsAlarmsList;
+}
+function customerAccountChange(event){
+    $("#getAlarmsData").attr("disabled",false);
+}
+function createTable(alarms) {
+    const table = $('#alarmsList table');
+    
+    // Clear existing table content
+    table.empty();
+    
+    // Create table header
+    const headers = Object.keys(alarms[0]);
+    let headerHtml = '<thead><tr>';
+    
+    headers.forEach(headerText => {
+        headerHtml += `<th>${headerText}</th>`;
+    });
+    
+    headerHtml += '</tr></thead>';
+    table.append(headerHtml);
+    
+    // Create table body
+    let bodyHtml = '<tbody>';
+    
+    alarms.forEach(alarm => {
+        bodyHtml += '<tr>';
+        headers.forEach(header => {
+            console.log(alarm[header].toLowerCase());
+            if (header.toLowerCase() === 'state' && alarm[header].toLowerCase() === 'in alarm') {
+                bodyHtml += `<td class="red">${alarm[header]}</td>`;
+            } else {
+                bodyHtml += `<td>${alarm[header]}</td>`;
+            }
+        });
+        bodyHtml += '</tr>';
+    });
+    
+    bodyHtml += '</tbody>';
+    table.append(bodyHtml);
+}
+
+async function getAlarmsData(){
+    input = $("#customerAccounts").val();
+    const accounts = getAccountsAlarmsAPI();
+
+    let apiURL = accounts
+    .filter(account => account[input])
+    .map(account => account[input].cloudWatchAPI)[0];
+    try{
+        let response = await fetch(apiURL, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (!response.ok) {
+            console.log(response);
+        } else {
+            let alarms = await response.json();
+            console.log("alarms",alarms);
+            // alarms = [{"AlarmName": "Alarm_ConcurrentCalls", "State": "ALARM", "Metric": "ConcurrentCalls", "Namespace": "AWS/Connect", "Description": "No description"}, {"AlarmName": "Alarm_callsperinterval", "State": "INSUFFICIENT_DATA", "Metric": "CallsPerInterval", "Namespace": "AWS/Connect", "Description": "No description"}, {"AlarmName": "Alarm_missedcalls", "State": "INSUFFICIENT_DATA", "Metric": "MissedCalls", "Namespace": "AWS/Connect", "Description": "No description"}];
+            createTable(alarms);
+        }
+    } catch(err){
+        console.log(err);
+    }
+
+}
