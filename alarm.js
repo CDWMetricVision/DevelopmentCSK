920bbc33fea63439e5a134a5134619330b6be240
kaj1451 <kajendran.suyambulingam@cdw.com>
2025-02-12 12:34:50 +0530

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -1,3 +1,4 @@
+// Existing function: getAccountsAlarmsAPI
 function getAccountsAlarmsAPI() {
     const allAccountsAlarmsList = [
         {
@@ -18,9 +19,39 @@ function getAccountsAlarmsAPI() {
     ]
     return allAccountsAlarmsList;
 }
-function customerAccountChange(event){
-    $("#getAlarmsData").attr("disabled",false);
+
+// Function to fetch alarms data using access token
+function fetchAlarms() {
+    let accessToken = sessionStorage.getItem("MetricVisionAccessToken");
+
+    if (accessToken) {
+        fetch('/api/alarms', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // Process the alarms data from the /api/alarms endpoint
+            // Optionally, you can call createTable with this data
+            createTable(data);
+        })
+        .catch(error => {
+            console.error('Error fetching alarms:', error);
+        });
+    } else {
+        alert('Access token not found. Please sign in again.');
+    }
 }
+
+// Existing function: customerAccountChange
+function customerAccountChange(event) {
+    $("#getAlarmsData").attr("disabled", false);
+}
+
+// Existing function: createTable
 function createTable(alarms) {
     const table = $('#alarmsList table');
     
@@ -58,30 +89,29 @@ function createTable(alarms) {
     table.append(bodyHtml);
 }
 
-async function getAlarmsData(){
+// Function to get alarms data from CloudWatch API for selected account
+async function getAlarmsData() {
     input = $("#customerAccounts").val();
     const accounts = getAccountsAlarmsAPI();
 
     let apiURL = accounts
     .filter(account => account[input])
     .map(account => account[input].cloudWatchAPI)[0];
-    try{
-        await fetch(apiURL).then(response =>{
+    try {
+        await fetch(apiURL).then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             return response.json();
-            }).then(data=>{
-                const body = JSON.parse(data.body); // Parse the body string into an array of objects
-                console.log(body);
-                createTable(body);
-
-            })
-            .catch(error =>{
-                console.error('There was a problem with the fetch operation:', error);
-            });
-    } catch(err){
+        }).then(data => {
+            const body = JSON.parse(data.body); // Parse the body string into an array of objects
+            console.log(body);
+            createTable(body);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+    } catch (err) {
         console.log(err);
     }
-
 }
