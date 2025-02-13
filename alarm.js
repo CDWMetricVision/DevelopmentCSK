d2f72d99c1a6bac7b42d65bab70cfa121d199d0e
kaj1451 <kajendran.suyambulingam@cdw.com>
2025-02-13 11:53:47 +0530

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -1,32 +1,43 @@
+// Function to handle when the page is loaded
+window.onload = () => {
+    if (window.location.hash) {
+        let hash = window.location.hash;
+        let token = hash.split("access_token=")[1].split("&")[0];
+        sessionStorage.setItem("MetricVisionAccessToken", token);
+    }
+}
 
-// Existing function: getAccountsAlarmsAPI
+// Function to get available accounts and their respective API URLs
 function getAccountsAlarmsAPI() {
     const allAccountsAlarmsList = [
         {
             "MAS Sandbox Development": {
-                "cloudWatchAPI":"https://szw9nl20j5.execute-api.us-east-1.amazonaws.com/test/getalarm"
+                "cloudWatchAPI": "https://szw9nl20j5.execute-api.us-east-1.amazonaws.com/test/getalarm"
             }
         },
         {
             "MAS Sandbox Test1": {
-                "cloudWatchAPI":"https://8vauowiu26.execute-api.us-east-1.amazonaws.com/test/getalarm"
+                "cloudWatchAPI": "https://8vauowiu26.execute-api.us-east-1.amazonaws.com/test/getalarm"
             }
         },
         {
             "MAS Sandbox Test2": {
-                "cloudWatchAPI":"https://9v5jzdmc6a.execute-api.us-east-1.amazonaws.com/test/getalarm"
+                "cloudWatchAPI": "https://9v5jzdmc6a.execute-api.us-east-1.amazonaws.com/test/getalarm"
             }
         }
     ]
     return allAccountsAlarmsList;
 }
 
-// Existing function: customerAccountChange
+// Updated customerAccountChange to automatically trigger fetch when account is selected
 function customerAccountChange(event) {
-    $("#getAlarmsData").attr("disabled", false);
+    const selectedAccount = event.target.value;
+    if (selectedAccount) {
+        getAlarmsData(selectedAccount);
+    }
 }
 
-// Existing function: createTable
+// Function to create a table from the alarms data
 function createTable(alarms) {
     const table = $('#alarmsList table');
     
@@ -64,16 +75,13 @@ function createTable(alarms) {
     table.append(bodyHtml);
 }
 
-// Function to get alarms data from CloudWatch API for selected account
-
-// Function to get alarms data from CloudWatch API for selected account
-async function getAlarmsData() {
-    input = $("#customerAccounts").val();
+// Function to get alarms data from CloudWatch API for the selected account
+async function getAlarmsData(selectedAccount) {
     const accounts = getAccountsAlarmsAPI();
 
     let apiURL = accounts
-    .filter(account => account[input])
-    .map(account => account[input].cloudWatchAPI)[0];
+        .filter(account => account[selectedAccount])
+        .map(account => account[selectedAccount].cloudWatchAPI)[0];
 
     // Get the access token from sessionStorage
     const token = sessionStorage.getItem("MetricVisionAccessToken");
